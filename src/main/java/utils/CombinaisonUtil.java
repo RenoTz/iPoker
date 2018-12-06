4a1bffad0b6eb07bc94ff1230a96f91d280128ec
Renaud Trezieres <renaud.trezieres@capgemini.com>
2018-12-06 16:47:27 +0100

    add combinaison : quinte / couleur

--- src/main/java/utils/CombinaisonUtil.java
+++ src/main/java/utils/CombinaisonUtil.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import com.google.common.collect.Maps;
 
 import model.Carte;
+import model.ColorEnum;
 
 public class CombinaisonUtil {
 
@@ -31,10 +32,45 @@ public class CombinaisonUtil {
 		return hasBrelan(cartes) && hasPaire(cartes);
 	}
 
+	public static boolean hasCouleur(final List<Carte> cartes) {
+
+		final List<ColorEnum> couleurs = getCouleursCartesTriees(cartes);
+
+		final Map<ColorEnum, Integer> cartesDeMemeCouleur = Maps.newHashMap();
+		for (final ColorEnum c : couleurs) {
+			if (cartesDeMemeCouleur.containsKey(c)) {
+				cartesDeMemeCouleur.put(c, cartesDeMemeCouleur.get(c) + 1);
+			} else {
+				cartesDeMemeCouleur.put(c, 1);
+			}
+		}
+
+		return couleurs.stream().anyMatch(c -> cartesDeMemeCouleur.get(c) >= 5);
+	}
+
+	public static boolean hasQuinte(final List<Carte> cartes) {
+
+		final List<Integer> valeurs = getValeursCartesTriees(cartes);
+
+		int i = 1;
+		int valeurPrecedente = -1;
+		for (final Integer v : valeurs) {
+			if (valeurPrecedente + 1 == v) {
+				i++;
+			}
+			valeurPrecedente = v;
+		}
+		return i >= 5;
+	}
+
+	public static boolean hasFullHouse(final List<Carte> cartes) {
+
+		return false;
+	}
+
 	private static boolean hasNombreCartesSouhaite(final List<Carte> cartes, final int nbCarteSouhaite) {
 
-		final List<Integer> valeurs = cartes.stream().map(Carte::getValeur).collect(Collectors.toList());
-		Collections.sort(valeurs);
+		final List<Integer> valeurs = getValeursCartesTriees(cartes);
 
 		final Map<Integer, Integer> cartesIdentiques = Maps.newHashMap();
 		for (final Integer v : valeurs) {
@@ -46,6 +82,20 @@ public class CombinaisonUtil {
 		}
 
 		return valeurs.stream().anyMatch(v -> cartesIdentiques.get(v) == nbCarteSouhaite);
+	}
+
+	private static List<Integer> getValeursCartesTriees(final List<Carte> cartes) {
+
+		final List<Integer> valeurs = cartes.stream().map(Carte::getValeur).collect(Collectors.toList());
+		Collections.sort(valeurs);
+		return valeurs;
+	}
+
+	private static List<ColorEnum> getCouleursCartesTriees(final List<Carte> cartes) {
+
+		final List<ColorEnum> couleurs = cartes.stream().map(Carte::getCouleur).collect(Collectors.toList());
+		Collections.sort(couleurs);
+		return couleurs;
 	}
 
 }
--- src/main/java/utils/Test.java
+++ src/main/java/utils/Test.java
@@ -23,6 +23,30 @@ public class Test {
 		System.out.println("a un carré : " + CombinaisonUtil.hasCarre(cartes));
 		System.out.println("a un full : " + CombinaisonUtil.hasFull(cartes));
 
+		// test main : couleur
+		final Carte dixCarreau = new Carte(10, "Dix", ColorEnum.CARREAU);
+		final Carte troisCarreau = new Carte(3, "Trois", ColorEnum.CARREAU);
+		final Carte quatreCarreau = new Carte(4, "Quatre", ColorEnum.CARREAU);
+		final Carte valetPique = new Carte(11, "Valet", ColorEnum.PIQUE);
+		final Carte cinqCarreau = new Carte(1, "Cinq", ColorEnum.CARREAU);
+		final Carte roisCarreau = new Carte(12, "Roi", ColorEnum.CARREAU);
+		final List<Carte> cartesCouleur = Lists.newArrayList(dixCarreau, troisCarreau, quatreCarreau, valetPique,
+				cinqCarreau, roisCarreau);
+
+		System.out.println("a une couleur : " + CombinaisonUtil.hasCouleur(cartesCouleur));
+
+		// test : quinte
+		final Carte septPique = new Carte(7, "Sept", ColorEnum.PIQUE);
+		final Carte huitCarreau = new Carte(8, "Huit", ColorEnum.CARREAU);
+		final Carte troisTrefle = new Carte(3, "Trois", ColorEnum.TREFLE);
+		final Carte neufCarreau = new Carte(9, "Neuf", ColorEnum.CARREAU);
+		final Carte damePique = new Carte(10, "Dame", ColorEnum.PIQUE);
+		final Carte valetCarreau = new Carte(11, "Valet", ColorEnum.CARREAU);
+
+		final List<Carte> cartesQuinte = Lists.newArrayList(septPique, huitCarreau, troisTrefle, neufCarreau, damePique,
+				valetCarreau);
+
+		System.out.println("a une quinte : " + CombinaisonUtil.hasQuinte(cartesQuinte));
 	}
 
 }
