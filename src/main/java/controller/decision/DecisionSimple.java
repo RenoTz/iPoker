3c3bee8dfc5d9ff1af14ea78d60d4d8ba115621e
Renaud Trezieres <renaud.trezieres@capgemini.com>
2018-12-21 17:07:26 +0100

    debut d'implementation de prise de decision

--- src/main/java/controller/decision/DecisionSimple.java
+++ src/main/java/controller/decision/DecisionSimple.java
@@ -1,11 +1,25 @@
 package controller.decision;
 
+import java.util.List;
+
+import model.ActionJoueurEnum;
+import model.Carte;
+import model.CombinaisonEnum;
+import model.Joueur;
+
 public class DecisionSimple implements InterfaceDecision {
 
 	@Override
-	public boolean deciderDeMiser() {
+	public boolean deciderDeMiser(final List<Joueur> joueurs, final List<Carte> cartesVisibles,
+			final boolean premierJoueur) {
 
-		// TODO Auto-generated method stub
+		for (final Joueur joueur : joueurs) {
+			if (joueur.getActionJoueur() == ActionJoueurEnum.EN_ATTENTE
+					&& joueur.getCarteCombinaison().getCombinaison() != CombinaisonEnum.HAUTEUR) {
+				joueur.getActionJoueur().actionSuivante(ActionJoueurEnum.SUIVRE, premierJoueur);
+				return true;
+			}
+		}
 		return false;
 	}
 
--- src/main/java/controller/decision/InterfaceDecision.java
+++ src/main/java/controller/decision/InterfaceDecision.java
@@ -1,7 +1,12 @@
 package controller.decision;
 
+import java.util.List;
+
+import model.Carte;
+import model.Joueur;
+
 public interface InterfaceDecision {
 
-	boolean deciderDeMiser();
+	boolean deciderDeMiser(final List<Joueur> joueurs, final List<Carte> cartesVisibles, final boolean premierJoueur);
 
 }
